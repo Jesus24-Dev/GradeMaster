f4adcc9dc33e5928d30d14e78853b774c56d9cff
Jesus24-Dev <siritjesus24@gmail.com>
2024-11-26 19:48:39 -0400

    adding enums

---
+++ src/main/java/grademaster/utils/StudyEnums.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grademaster.utils;
+
+/**
+ *
+ * @author Jesus24-Dev
+ */
+public class StudyEnums {
+    public static enum YearStudy {
+        FIRST_YEAR,
+        SECOND_YEAR,
+        THIRD_YEAR,
+        FOURTH_YEAR,
+        FIFTH_YEAR
+    }
+    
+    public static enum SectionStudy{
+        SECTION_A,
+        SECTION_B,
+        SECTION_C,
+        SECTION_D
+    }
+    
+    public static enum Test {
+        TEST_1,
+        TEST_2,
+        TEST_3,
+        TEST_4,
+        TEST_5
+    }
+    
+    public static enum Period {
+        FIRST_PERIOD,
+        SECOND_PERIOD,
+        THIRD_PERIOD
+    }
+}
