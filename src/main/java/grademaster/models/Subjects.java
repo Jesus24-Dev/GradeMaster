1d879ea5f943cf59355bc2f3690f0d7f43f95a5a
Jesus24-Dev <siritjesus24@gmail.com>
2024-11-26 20:20:03 -0400

    Class subjects done

---
+++ src/main/java/grademaster/models/Subjects.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grademaster.models;
+
+/**
+ *
+ * @author Jesus24-Dev
+ */
+public class Subjects {
+    private int id;
+    private String name;
+
+    public Subjects(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
