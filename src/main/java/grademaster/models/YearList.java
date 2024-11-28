0d0aa59d8a76c7b4a95de23297ef5de8105b5f35
Jesus24-Dev <siritjesus24@gmail.com>
2024-11-28 14:09:43 -0400

    Year list model class created;

---
+++ src/main/java/grademaster/models/YearList.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grademaster.models;
+
+/**
+ *
+ * @author Jesus24-Dev
+ */
+public class YearList {
+    
+}
