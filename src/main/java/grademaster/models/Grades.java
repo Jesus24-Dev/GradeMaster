39cae4ab7e06601197ea7b7d1de1601d7604b025
Jesus24-Dev <siritjesus24@gmail.com>
2024-11-28 14:13:38 -0400

    Grades model class created

---
+++ src/main/java/grademaster/models/Grades.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grademaster.models;
+
+import grademaster.utils.StudyEnums.*;
+
+/**
+ *
+ * @author Jesus24-Dev
+ */
+public class Grades {
+    private String studentId ;
+    private int subjectId; 
+    private YearStudy yearStudy;
+    private Test activity;
+    private Period period;
+    private float grade;
+
+    public Grades(String studentId, int subjectId, YearStudy yearStudy, Test activity, Period period, float grade) {
+        this.studentId = studentId;
+        this.subjectId = subjectId;
+        this.yearStudy = yearStudy;
+        this.activity = activity;
+        this.period = period;
+        this.grade = grade;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public YearStudy getYearStudy() {
+        return yearStudy;
+    }
+
+    public void setYearStudy(YearStudy yearStudy) {
+        this.yearStudy = yearStudy;
+    }
+
+    public Test getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Test activity) {
+        this.activity = activity;
+    }
+
+    public Period getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+    
+    
+}
