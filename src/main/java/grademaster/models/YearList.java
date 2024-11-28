d5f85d68d3946e05d34ae90c38341b724e2035e5
Jesus24-Dev <siritjesus24@gmail.com>
2024-11-28 14:10:34 -0400

    Year list model class created

--- src/main/java/grademaster/models/YearList.java
+++ src/main/java/grademaster/models/YearList.java
@@ -4,10 +4,46 @@
  */
 package grademaster.models;
 
+import grademaster.utils.StudyEnums.SectionStudy;
+import grademaster.utils.StudyEnums.YearStudy;
+
 /**
  *
  * @author Jesus24-Dev
  */
 public class YearList {
-    
+    private String studentId;
+    private YearStudy yearStudy;
+    private SectionStudy section;
+
+    public YearList(String studentId, YearStudy yearStudy, SectionStudy section) {
+        this.studentId = studentId;
+        this.yearStudy = yearStudy;
+        this.section = section;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public YearStudy getYearStudy() {
+        return yearStudy;
+    }
+
+    public void setYearStudy(YearStudy yearStudy) {
+        this.yearStudy = yearStudy;
+    }
+
+    public SectionStudy getSection() {
+        return section;
+    }
+
+    public void setSection(SectionStudy section) {
+        this.section = section;
+    }
+        
 }
