698d736b68775efd9f2f120236257c95d8202122
Jesus24-Dev <siritjesus24@gmail.com>
2024-12-11 12:20:54 -0400

    Added read by subject and read by student

--- src/main/java/grademaster/repository/GradesRepository.java
+++ src/main/java/grademaster/repository/GradesRepository.java
@@ -53,4 +53,74 @@ public class GradesRepository {
             return null;
         }
     }
+    
+    public ArrayList<Grades> getGradesByStudent(Connection conn, String id){
+        ArrayList<Grades> grades = new ArrayList<>();
+        String query = """
+                       SELECT a.id, a.name, a.lastname, b.namesubject, c.yearstudy, c.activity, c.periodtest, c.sectionstudy, c.grade
+                       FROM grades c
+                       INNER JOIN  users a on c.studentid = a.id
+                       INNER JOIN subject b on b.id = c.subjectid
+                       WHERE c.studentid = ?
+                       """;
+        try(PreparedStatement pstm = conn.prepareStatement(query)){
+            pstm.setString(1, id);
+            ResultSet rs = pstm.executeQuery();
+                if(rs.next()){
+                    do {
+                    Grades grade = new Grades(
+                            rs.getString("id"),
+                            rs.getString("name"),
+                            rs.getString("lastname"),
+                            rs.getString("namesubject"),
+                            YearStudy.valueOf(rs.getString("yearstudy")),
+                            Test.valueOf(rs.getString("activity")),
+                            Period.valueOf(rs.getString("periodtest")),
+                            SectionStudy.valueOf(rs.getString("sectionstudy")),
+                            rs.getFloat("grade")
+                    ); 
+                    grades.add(grade);
+                } while(rs.next());
+            }
+            return grades;
+        } catch (SQLException e){
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+    public ArrayList<Grades> getGradesBySubject(Connection conn, String subjectName){
+        ArrayList<Grades> grades = new ArrayList<>();
+        String query = """
+                       SELECT a.id, a.name, a.lastname, b.namesubject, c.yearstudy, c.activity, c.periodtest, c.sectionstudy, c.grade
+                       FROM grades c
+                       INNER JOIN  users a on c.studentid = a.id
+                       INNER JOIN subject b on b.id = c.subjectid
+                       WHERE c.subjectid = (SELECT id FROM subject WHERE namesubject = ?)
+                       """;
+        try(PreparedStatement pstm = conn.prepareStatement(query)){
+            pstm.setString(1, subjectName);
+            ResultSet rs = pstm.executeQuery();
+                if(rs.next()){
+                    do {
+                    Grades grade = new Grades(
+                            rs.getString("id"),
+                            rs.getString("name"),
+                            rs.getString("lastname"),
+                            rs.getString("namesubject"),
+                            YearStudy.valueOf(rs.getString("yearstudy")),
+                            Test.valueOf(rs.getString("activity")),
+                            Period.valueOf(rs.getString("periodtest")),
+                            SectionStudy.valueOf(rs.getString("sectionstudy")),
+                            rs.getFloat("grade")
+                    ); 
+                    grades.add(grade);
+                } while(rs.next());
+            }
+            return grades;
+        } catch (SQLException e){
+            System.out.println(e);
+            return null;
+        }
+    }
 }
