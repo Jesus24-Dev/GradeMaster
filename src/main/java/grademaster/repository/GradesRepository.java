0d0b820be2543df455ff22adf1ca6fcf09c2012c
Jesus24-Dev <siritjesus24@gmail.com>
2024-12-11 12:13:59 -0400

    new file gradesrepository. Added get grades method

---
+++ src/main/java/grademaster/repository/GradesRepository.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grademaster.repository;
+
+import grademaster.models.Grades;
+import grademaster.utils.StudyEnums.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jesus24-Dev
+ */
+public class GradesRepository {
+    public ArrayList<Grades> getGrades(Connection conn, YearStudy yearStudy, SectionStudy sectionStudy){
+        ArrayList<Grades> grades = new ArrayList<>();
+        String query = """
+                       SELECT a.id, a.name, a.lastname, b.namesubject, c.yearstudy, c.activity, c.periodtest, c.sectionstudy, c.grade
+                       FROM grades c
+                       INNER JOIN  users a on c.studentid = a.id
+                       INNER JOIN subject b on b.id = c.subjectid
+                       WHERE c.yearstudy = ? AND c.sectionstudy = ?
+                       """;
+        try(PreparedStatement pstm = conn.prepareStatement(query)){
+            pstm.setString(1, yearStudy.toString());
+            pstm.setString(2, sectionStudy.toString());
+            
+            ResultSet rs = pstm.executeQuery();
+                if(rs.next()){
+                    do {
+                    Grades grade = new Grades(
+                            rs.getString("id"),
+                            rs.getString("name"),
+                            rs.getString("lastname"),
+                            rs.getString("namesubject"),
+                            YearStudy.valueOf(rs.getString("yearstudy")),
+                            Test.valueOf(rs.getString("activity")),
+                            Period.valueOf(rs.getString("periodtest")),
+                            SectionStudy.valueOf(rs.getString("sectionstudy")),
+                            rs.getFloat("grade")
+                    ); 
+                    grades.add(grade);
+                } while(rs.next());
+            }
+            return grades;
+        } catch (SQLException e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
