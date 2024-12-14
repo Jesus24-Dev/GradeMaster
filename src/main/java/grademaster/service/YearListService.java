a80d5bf709e05af94e8bf774dbc491ab4b7cce0c
Jesus24-Dev <siritjesus24@gmail.com>
2024-12-14 11:03:09 -0400

    Created YearList service

---
+++ src/main/java/grademaster/service/YearListService.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grademaster.service;
+
+import grademaster.models.YearList;
+import grademaster.repository.YearListRepository;
+import grademaster.utils.StudyEnums.SectionStudy;
+import grademaster.utils.StudyEnums.YearStudy;
+import java.sql.Connection;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jesus24-Dev
+ */
+public class YearListService {
+    private final YearListRepository yearListRepository;
+    private final Connection conn;
+
+    public YearListService(YearListRepository yearListRepository, Connection conn) {
+        this.yearListRepository = yearListRepository;
+        this.conn = conn;
+    }
+    
+    public ArrayList<YearList> getList(Connection conn, YearStudy yearStudy, SectionStudy sectionStudy){
+        return yearListRepository.getYearList(conn, yearStudy, sectionStudy);
+    }
+    
+    public void addStudentToList(Connection conn, YearList yearList){
+        yearListRepository.addStudentToList(conn, yearList);
+    }
+    
+    public void updateList(Connection conn, YearList yearList){
+        yearListRepository.updateList(conn, yearList);
+    }
+    
+    public void deleteFromList(Connection conn, String id){
+        yearListRepository.deleteFromList(conn, id);
+    }
+}
