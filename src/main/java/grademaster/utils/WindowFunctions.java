38dd9049b84157408f922d4128eac5356c2fd818
Jesus24-Dev <siritjesus24@gmail.com>
2024-12-28 19:48:46 -0400

    change the method to specific methods

--- src/main/java/grademaster/utils/WindowFunctions.java
+++ src/main/java/grademaster/utils/WindowFunctions.java
@@ -1,14 +1,34 @@
 
 package grademaster.utils;
 
+import grademaster.views.main.WindowForm;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 
 public class WindowFunctions {
-    public static void changePanel(JPanel panelMain, JPanel panelToChange){
-        panelMain.removeAll();
-        panelMain.add(panelToChange, BorderLayout.CENTER);
-        panelMain.revalidate();
-        panelMain.repaint();
+    private static WindowForm windowForm = new WindowForm();
+    private static JPanel content = windowForm.getContent();
+    private static JPanel navbar = windowForm.getNavbar();
+    
+    public static void changeContent(JPanel panelToChange){
+        content.removeAll();
+        content.add(panelToChange, BorderLayout.CENTER);
+        content.revalidate();
+        content.repaint();
+    }
+    
+    public static void changeNavbar(JPanel navbarToChange){
+        navbar.removeAll();
+        navbar.add(navbarToChange, BorderLayout.CENTER);
+        navbar.revalidate();
+        navbar.repaint();
+    }
+    
+    public static void changeTitle(String title){
+        windowForm.setTitle(title);
+    }
+    
+    public static void setWindowVisible(){
+        windowForm.setVisible(true);
     }
 }
